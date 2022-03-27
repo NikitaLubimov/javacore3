3ee1158f46774bd60267d3dde7c22c053cab34df
NikitaLubimov <ikarus11122@gmail.com>
2022-03-27 20:54:49 +0300

    hw4

--- src/javacore3GB/Main.java
+++ src/javacore3GB/Main.java
@@ -1,8 +1,63 @@
 package javacore3GB;
 
 public class Main {
+    static Object mon = new Object();
+    static volatile String str = "A";
+    static final int num = 5;
+
 
     public static void main(String[] args) {
-	// write your code here
+        new Thread(() -> {
+            try {
+                for (int i = 0; i <= num; i++) {
+                    synchronized (mon) {
+                        if (str.equals("A")) {
+                            System.out.print("A");
+                            str = "B";
+                            mon.notifyAll();
+                        }
+                        mon.wait();
+                    }
+                }
+            } catch ( InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i <= num; i++) {
+                    synchronized (mon) {
+                        if (str.equals("B")) {
+                            System.out.print("B");
+                            str = "C";
+                            mon.notifyAll();
+                        }
+                        mon.wait();
+                    }
+                }
+            } catch ( InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i <= num; i++) {
+                    synchronized (mon) {
+                        if (str.equals("C")) {
+                            System.out.print("C");
+                            str = "A";
+                            mon.notifyAll();
+                        }
+                        mon.wait();
+                    }
+                }
+            } catch ( InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
+
+
